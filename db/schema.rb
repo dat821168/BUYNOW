115f87d60d22723b092625a5cfcc1bea6fe1af3e
Trọng Đắt Ngô <dat821168@gmail.com>
2018-05-18 16:48:47 +0700

    final database 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_062146) do
+ActiveRecord::Schema.define(version: 2018_05_18_094740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_05_18_062146) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -104,6 +105,7 @@ ActiveRecord::Schema.define(version: 2018_05_18_062146) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.integer "quantity"
+    t.string "name"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["producer_id"], name: "index_products_on_producer_id"
   end
